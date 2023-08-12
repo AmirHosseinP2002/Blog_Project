3acdc34a514c2945f20ae04e7b5d801b127f3bc1
AmirHosseinP2002 <amirhossein.poursaeed1@gmail.com>
2023-08-12 13:11:56 +0330

    add search list

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
+from django.db.models import Q
 
 from .models import Article
 from .forms import ArticleCreateForm
@@ -32,7 +35,17 @@ class ArticleUpdateView(generic.UpdateView):
     form_class = ArticleCreateForm
     template_name = 'blog/article_create_update.html'
 
+
 class ArticleDeleteView(generic.DeleteView):
     model = Article
     template_name = 'blog/article_delete.html'
     success_url = reverse_lazy('blog:article_list')
+
+
+class ArticlesearchListView(generic.ListView):
+    template_name = 'blog/article_search.html'
+    context_object_name = 'articles'
+
+    def get_queryset(self):
+        q = self.request.GET.get('q')
+        return Article.objects.filter(Q(title__icontains=q) | Q(description__icontains=q))
